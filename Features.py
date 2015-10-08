75b947742be3157551dfcbd2e26f221b0e2987cc
Roy Lowrance <roy.lowrance@gmail.com>
2015-10-08 15:15:04 -0400

    fix propn-derived features

--- Features.py
+++ Features.py
@@ -1,3 +1,4 @@
+import layout_parcels as parcels
 import layout_transactions as t
 
 
@@ -18,7 +19,7 @@ class Features(object):
         Used to build the X array used for training
         The X array columns are in the order.
         '''
-        return (  # 28 features (added has_commercial)
+        non_geo_features = [
             (t.age, None),
             (t.age2, None),
             (t.age_effective, None),
@@ -27,7 +28,6 @@ class Features(object):
             (t.building_baths, 'log1p'),
             (t.building_bedrooms, 'log1p'),
             (t.building_fireplace_number, 'log1p'),
-            (t.building_has_pool, None),
             (t.building_is_new_construction, None),
             (t.building_living_square_feet, 'log'),
             (t.building_rooms, 'log1p'),
@@ -35,19 +35,22 @@ class Features(object):
             (t.census2000_fraction_owner_occupied, None),
             (t.census2000_median_household_income, 'log'),
             (t.census2000_avg_commute, None),
-            (t.census_tract_has_commercial, None),
-            (t.census_tract_has_industry, None),
-            (t.census_tract_has_park, None),
-            (t.census_tract_has_retail, None),
-            (t.census_tract_has_school, None),
+            (t.has_pool, None),
             (t.lot_land_square_feet, 'log'),
             (t.lot_parking_spaces, 'log1p'),
-            (t.zip5_has_commercial, None),
-            (t.zip5_has_industry, None),
-            (t.zip5_has_retail, None),
-            (t.zip5_has_park, None),
-            (t.zip5_has_school, None),
-        )
+        ]
+        # these are all indicator variables (0 or 1)
+        census_tract_propn = [('census_tract_has_' + x, None) for x in parcels.propn.keys()]
+        zip5_propn = [('zip5_has_' + x, None) for x in parcels.propn.keys()]
+        aggregated_features = ('any_commercial', 'any_industrial', 'any_non_residential')
+        census_tract_aggregated = [('census_tract_has_' + x, None) for x in aggregated_features]
+        zip5_aggregated = [('zip5_has_' + x, None) for x in aggregated_features]
+        result = tuple(non_geo_features +
+                       census_tract_propn +
+                       zip5_propn +
+                       census_tract_aggregated +
+                       zip5_aggregated)
+        return result
         return (  # 26 features in OLD version
             ('fraction.owner.occupied', None),
             ('FIREPLACE.NUMBER', 'log1p'),
