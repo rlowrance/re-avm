1c38a95c98a8a91131a432016aa61e57761b878e
Roy Lowrance <roy.lowrance@gmail.com>
2015-10-21 20:35:53 -0400

    refactor to reuse code

--- AVM.py
+++ AVM.py
@@ -17,6 +17,7 @@ class AVM(sklearn.base.BaseEstimator):
     'one estimator for two underlying models: ElasticNet and RandomForestRegressor'
     def __init__(self,
                  model_name=None,          # parameters for all models
+                 forecast_time_period=None,
                  n_months_back=None,
                  random_state=None,
                  alpha=None,               # for ElasticNet
@@ -29,6 +30,7 @@ class AVM(sklearn.base.BaseEstimator):
         # NOTE: just capture the parameters (to conform to the sklearn protocol)
         # hyperparameters for linear models
         self.model_name = model_name
+        self.forecast_time_period = forecast_time_period
         self.n_months_back = n_months_back
         self.units_X = units_X
         self.units_y = units_y
@@ -39,7 +41,7 @@ class AVM(sklearn.base.BaseEstimator):
 
     def fit(self, df):
         'construct and fit df that contains X and y'
-        def fit_elastic_net(df):
+        def fit_elastic_net(X_train, y_train):
             print 'fit elastic net: %s~%s alpha: %f l1_ratio: %f' % (
                 self.units_X, self.units_y, self.alpha, self.l1_ratio)
 
@@ -52,13 +54,10 @@ class AVM(sklearn.base.BaseEstimator):
                 selection='random',
                 random_state=self.random_state,
             )
-            # TODO: handle fewer observations than features (see AM)
-            X_train, y_train = self.extract_and_transform(df)
             self.model.fit(X_train, y_train)
-            # TODO: save the attributes that the above statement generated
             return self
 
-        def fit_random_forest_regressor(df):
+        def fit_random_forest_regressor(X_train, y_train):
             print 'fit random forest regressor: n_estimators: %d max_depth: %d' % (
                 self.n_estimators, self.max_depth)
             self.model = sklearn.ensemble.RandomForestRegressor(
@@ -66,14 +65,25 @@ class AVM(sklearn.base.BaseEstimator):
                 max_depth=self.max_depth,
                 random_state=self.random_state,
             )
-            X_train, y_train = self.extract_and_transform(df)
             self.model.fit(X_train, y_train)
             return self
 
+        mask = df[layout_transactions.yyyymm] < self.forecast_time_period
+        df_period = df[mask]
+        kept_yyyymm = []
+        last_kept_yyyymm = self.forecast_time_period - 1
+        for n in xrange(self.n_months_back):
+            if last_kept_yyyymm % 100 == 0:
+                # ex: found 200900; need to convert to 200812
+                last_kept_yyyymm += - 100 + 12  # adjust year and month
+            kept_yyyymm.append(last_kept_yyyymm)
+        mask_kept = df_period[layout_transactions.yyyymm].isin(kept_yyyymm)
+        df_kept = df_period[mask_kept]
+        X_train, y_train = self.extract_and_transform(df_kept)
         return {
             'ElasticNet': fit_elastic_net,
             'RandomForestRegressor': fit_random_forest_regressor,
-        }[self.model_name](df)
+        }[self.model_name](X_train, y_train)
 
     def get_attributes(self):
         'return both sets of attributes, with None if not used by that model'
