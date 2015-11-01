21e5ea8ad4d779a0bce8be47958b3949e52dd521
Roy Lowrance <roy.lowrance@gmail.com>
2015-11-01 10:01:41 -0500

    --test has usable output; for 1 year

--- chart-02.py
+++ chart-02.py
@@ -68,7 +68,7 @@ def make_control(argv):
         debug=debug,
         path_in_ege=dir_working + 'ege-rfbound-*-folds-10.pickle',
         path_out_txt_base=dir_working + out_file_name_base,
-        path_data=dir_working + out_file_name_base + '.data.pickle',
+        path_data=dir_working + arg.base_name + '.data.pickle',
         random_seed=random_seed,
         test=arg.test,
     )
@@ -79,93 +79,131 @@ def make_chart(df, control, ege_control):
     format_header = '%12s %12s %12s'
     format_detail = '%12d %12d %12.0f'
 
-    def make_mean_loss(n_months_back, n_estimators, max_depth):
-        mask1 = df.n_months_back == n_months_back
-        mask2 = df.n_estimators == n_estimators
-        mask3 = df.max_depth == max_depth
-        mask = mask1 & mask2 & mask3
+    def make_mean_loss(test_period, n_months_back, n_estimators, max_depth):
+        mask = (
+            (df.test_period == test_period) &
+            (df.n_months_back == n_months_back) &
+            (df.n_estimators == n_estimators) &
+            (df.max_depth == max_depth)
+        )
         selected = df.loc[mask]
         if len(selected) != 1:
             pdb.set_trace()
         assert len(selected) == 1, len(selected)
-        mean_loss = - selected.mean_validation_score
+        mean_loss = - selected.mean_loss
         return mean_loss
 
-    def n_estimators(r, n_months_back):
+    def n_estimators(r, test_period, n_months_back):
         r.append(format_header % ('n_estimators', 'max_depth', 'mean_loss'))
         # rely on the fact that the grid search was a grid
         for n_estimators in sorted(set(df.n_estimators)):
             for max_depth in sorted(set(df.max_depth)):
                 mean_loss = make_mean_loss(
+                    test_period=test_period,
                     n_months_back=n_months_back,
                     n_estimators=n_estimators,
                     max_depth=max_depth,
                 )
                 r.append(format_detail % (n_estimators, max_depth, mean_loss))
 
-    def max_depth(r, n_months_back):
+    def make_report(test_period, n_months_back):
+        r = Report()
+        r.append('CHART 02')
+        r.append('MEAN LOSS FROM %d-FOLD CROSS VALIDATION' % ege_control.n_cv_folds)
+        r.append('TEST PERIOD: %s' % test_period)
+        r.append('NUMBER OF MONTHS OF TRAINING DATA: %d' % n_months_back)
+        r.append('')
+        n_estimators(r, test_period, n_months_back)
+        r.append('')
+        max_depth(r, test_period, n_months_back)
+        r.write(out_file_base(test_period, n_months_back) + '.txt')
+
+    def max_depth(r, test_period, n_months_back):
         r.append(format_header % ('max_depth', 'n_estimators', 'mean_loss'))
         # rely on the fact that the grid search was a grid
         for max_depth in sorted(set(df.max_depth)):
             for n_estimators in sorted(set(df.n_estimators)):
                 mean_loss = make_mean_loss(
+                    test_period=test_period,
                     n_months_back=n_months_back,
                     max_depth=max_depth,
                     n_estimators=n_estimators,
                 )
                 r.append(format_detail % (max_depth, n_estimators, mean_loss))
 
-    def make_plot(n_months_back):
-        pdb.set_trace()
+    def make_subplot(test_period, n_months_back):
+        'mutate the default axes'
         for i, n_estimators in enumerate(sorted(set(df.n_estimators))):
             mask = (
+                (df.test_period == test_period) &
                 (df.n_months_back == n_months_back) &
                 (df.n_estimators == n_estimators)
             )
             subset = df.loc[mask]
             x = subset.max_depth
-            y = -subset.mean_validation_score  # loss
-            # AM: the lines don't show up
-            plt.plot(y,
+            y = subset.mean_loss
+            plt.plot(y / 1000.0,
                      label=('n_estimators: %d' % n_estimators),
-                     linestyle=[':', '-.', '- -', '-'][i % 4],
-                     color='bgrcmybw'[i % 8],
+                     # linestyle='.,ov^<>'[i],
+                     linestyle=[':', '-.', '--', '-'][i % 4],
+                     color='bgrcmykw'[i % 8],
                      )
-        pdb.set_trace()
         print df.max_depth.max()
-        plt.axis([0, len(y), 0, -df.mean_validation_score.max() + 20000])
-        plt.xticks(range(len(y)), x.values)
-        plt.title('Loss by max_depth and n_estimators')
-        plt.xlabel('max_depth')
-        plt.ylabel('loss')
-        plt.legend(loc="best")
-        # plt.show()  NOTE: this blocks if not in interactive mode
+        # plt.axis([0, len(y), 0, df.mean_loss.max() + 20000])
+        plt.xticks(range(len(y)), x.values, size='xx-small', rotation='vertical')
+        plt.yticks(size='xx-small')
+        plt.title('%s %d' % (test_period, n_months_back))
+        # plt.legend(loc='best')
         return
-        # MAYBE: create an axes for each n_months_back (there will be 12 of them)
 
-    def out_file_base(n_months_back):
+    def out_file_base(test_period, n_months_back):
         return (
             control.path_out_txt_base +
-            ('-%d' % control.yyyymm) +
+            ('-%s' % test_period) +
             ('-n_months_back-%02d' % n_months_back))
 
-    pdb.set_trace()
-    for n_months_back in sorted(set(df.n_months_back)):
-        r = Report()
-        r.append('CHART 02')
-        r.append('MEAN LOSS FROM %d-FOLD CROSS VALIDATION' % ege_control.n_cv_folds)
-        r.append('NUMBER OF MONTHS OF TRAINING DATA: %d' % n_months_back)
-        r.append('')
-        n_estimators(r, n_months_back)
-        r.append('')
-        max_depth(r, n_months_back)
-        pdb.set_trace()
-        r.write(out_file_base(n_months_back) + '.txt')
+    produce_report = False
+    test_periods = [str(year * 100 + month)
+                    for year in (2004, 2005, 2006, 2007, 2008)
+                    for month in (2, 5, 8, 11)
+                    ]
+    test_periods.append('200902')
+    n_months_backs = sorted(set(df.n_months_back))
+    if control.test:
+        test_periods = test_periods[:4]
+        n_months_backs = n_months_backs[:6]
 
-        make_plot(n_months_back)  # value is return in module plt
-        pdb.set_trace()
-        plt.savefig(out_file_base(n_months_back) + '.pdf')
-        break  # while debugging
+    pdb.set_trace()
+    plt.figure()  # new figure
+    axes_number = 0
+    last_test_period_index = len(test_periods) - 1
+    last_n_months_back_index = len(n_months_backs) - 1
+    for test_period_index, test_period in enumerate(test_periods):
+        for n_months_back_index, n_months_back in enumerate(n_months_backs):
+            axes_number += 1
+            if produce_report:
+                make_report(test_period, n_months_back)
+
+            # create a subplot
+            # axes_number counts across rows
+            plt.subplot(len(test_periods), len(n_months_backs), axes_number)
+            make_subplot(test_period, n_months_back)
+            if test_period_index == last_test_period_index:
+                # annotate the bottom row only
+                if n_months_back_index == 0:
+                    plt.xlabel('max_depth')
+                    plt.ylabel('loss x $1000')
+                if n_months_back_index == last_n_months_back_index:
+                    plt.legend(loc='best', fontsize='xx-small')
+
+    if len(test_periods) == 4 and len(n_months_back == 6):
+        # testing
+        plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=1.0)
+    else:
+        plt.tight_layout(pad=0.1, w_pad=0.1, h_pad=0.1)
+
+    plt.savefig(out_file_base(test_period, n_months_back) + '.pdf')
+    plt.close()
 
 
 def make_data(control):
@@ -210,8 +248,8 @@ def make_data(control):
                     'max_depth': grid_score.parameters['max_depth'],
                     'n_estimators': grid_score.parameters['n_estimators'],
                     'n_months_back': grid_score.parameters['n_months_back'],
-                    'loss': grid_score.mean_validation_score,
-                    'std': np.std(grid_score.cv_validation_scores),
+                    'mean_loss': -grid_score.mean_validation_score,
+                    'std_loss': np.std(grid_score.cv_validation_scores),
                 }
             )
         if verbose:
