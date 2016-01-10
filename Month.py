7866392c8ef4902fed9d3163ebdc9658b0a8fa30
Roy Lowrance <roy.lowrance@gmail.com>
2016-01-10 12:32:29 -0500

    new: Month data type

---
+++ Month.py
@@ -0,0 +1,83 @@
+'A month, as in 200703; immutable'
+
+import pdb
+import unittest
+
+
+class Month(object):
+    def __init__(self, value1, value2=None):
+        '''Constructors
+           Month('200703')
+           Month(200703)
+           Month(2007, 3)
+        '''
+
+        if value2 is None:
+            if isinstance(value1, str):
+                self.year = int(value1[:4])
+                self.month = int(value1[4:])
+            elif isinstance(value1, int):
+                self.month = value1 % 100
+                self.year = (value1 - self.month) / 100
+        else:
+            self.year = int(value1)
+            self.month = int(value2)
+
+        # enforce invariant (other methods depend on this)
+        assert self.year > 0, self
+        assert 1 <= self.month <= 12, self
+
+    def __repr__(self):
+        return 'Month(year=%d, month=%d)' % (self.year, self.month)
+
+    def increment(self):
+        'return new Month one month after self'
+        if self.month == 12:
+            return Month(self.year + 1, 1)
+        else:
+            return Month(self.year, self.month + 1)
+
+    def decrement(self):
+        'return new Month one month before self'
+        if self.month == 1:
+            return Month(self.year - 1, 12)
+        else:
+            return Month(self.year, self.month - 1)
+
+    def as_str(self):
+        return '%04d%02d' % (self.year, self.month)
+
+    def as_int(self):
+        return self.year * 100 + self.month
+
+    def equal(self, other):
+        return self.year == other.year and self.month == other.month
+
+
+class TestMonth(unittest.TestCase):
+    def test_constructor(self):
+        self.assertTrue(Month('200703').equal(Month(2007, 03)))
+        self.assertTrue(Month(200703).equal(Month(2007, 03)))
+        self.assertTrue(Month(200712).equal(Month(2007, 12)))
+
+    def test_as_str(self):
+        self.assertTrue(Month(200703).as_str() == '200703')
+
+    def test_as_int(self):
+        self.assertTrue(Month(200703).as_int() == 200703)
+
+    def test_equal(self):
+        self.assertTrue(Month('200703').equal(Month(2007, 03)))
+
+    def test_increment(self):
+        self.assertTrue(Month(200701).increment().equal(Month(200702)))
+        self.assertTrue(Month(200712).increment().equal(Month(200801)))
+
+    def test_decrement(self):
+        self.assertTrue(Month(200701).decrement().equal(Month(200612)))
+        self.assertTrue(Month(200712).decrement().equal(Month(200711)))
+
+if __name__ == '__main__':
+    unittest.main()
+    if False:
+        pdb.set_trace()
