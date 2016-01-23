27c503aa34344221a66c7b550e1d91fa11ff6253
Roy Lowrance <roy.lowrance@gmail.com>
2016-01-23 11:39:20 -0500

    new: dictionary on disk

---
+++ DiskDictionary.py
@@ -0,0 +1,88 @@
+import os
+import pdb
+import cPickle as pickle
+import unittest
+
+
+class DiskDictionary(object):
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.f = None
+
+    def append(self, key, value):
+        'write key and value to disk'
+        if self.f is None:
+            self.f = open(self.filepath, 'wb')
+        pickle.dump(key, self.f)
+        pickle.dump(value, self.f)
+
+    def close(self):
+        if self.f is not None:
+            self.f.close()
+
+    def items(self):
+        'generate (key, value) pairs'
+        if self.f is not None:
+            raise RuntimeError('backing file already opened')
+        self.f = open(self.filepath, 'rb')
+        while True:
+            try:
+                key = pickle.load(self.f)
+                value = pickle.load(self.f)
+                yield key, value
+            except EOFError:
+                self.f.close()
+                break  # fall through and yield StopIteration
+        self.f.close()
+
+    def keyset(self):
+        result = set()
+        for k, v in self.items():
+            assert k not in result, 'duplicate key: %s' % str(k)
+            result.add(k)
+        return result
+
+    # implement with DiskDictionary(path) as dd:
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        if self.f is not None:
+            self.f.close()
+
+
+class Test(unittest.TestCase):
+    def test1(self):
+        path = '/tmp/DiskDictionary'
+        os.system('rm %s' % path)
+        dd = DiskDictionary('/tmp/DiskDictionary')
+        dd.append('key1', ['value1a', 27])
+        dd.close()
+        dd = DiskDictionary(path)
+        found = 0
+        for k, v in dd.items():
+            found += 1
+
+            self.assertTrue(isinstance(k, str))
+            self.assertEqual(k, 'key1')
+
+            self.assertTrue(isinstance(v, list))
+            self.assertEqual(v[0], 'value1a')
+            self.assertEqual(v[1], 27)
+        self.assertEqual(found, 1)
+        dd.close()
+
+    def test_keyset(self):
+        path = '/tmp/DiskDictionary'
+        with DiskDictionary(path) as dd:
+            dd.append('key1', ['value1', 1])
+            dd.append('key2', ['valye2', 2])
+        with DiskDictionary(path) as dd:
+            keyset = dd.keyset()
+            self.assertEqual(len(keyset), 2)
+
+
+if __name__ == '__main__':
+    unittest.main()
+    if False:
+        pdb
