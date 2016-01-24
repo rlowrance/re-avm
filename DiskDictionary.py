c5dab22c6aaf54edeb9ec4f12683654a226e9541
Roy Lowrance <roy.lowrance@gmail.com>
2016-01-24 14:06:29 -0500

    disk file contains records (key,value)

--- DiskDictionary.py
+++ DiskDictionary.py
@@ -13,8 +13,8 @@ class DiskDictionary(object):
         'write key and value to disk'
         if self.f is None:
             self.f = open(self.filepath, 'wb')
-        pickle.dump(key, self.f)
-        pickle.dump(value, self.f)
+        record = (key, value)
+        pickle.dump(record, self.f)
 
     def close(self):
         if self.f is not None:
@@ -27,8 +27,8 @@ class DiskDictionary(object):
         self.f = open(self.filepath, 'rb')
         while True:
             try:
-                key = pickle.load(self.f)
-                value = pickle.load(self.f)
+                record = pickle.load(self.f)
+                key, value = record
                 yield key, value
             except EOFError:
                 self.f.close()
@@ -41,10 +41,11 @@ class DiskDictionary(object):
         return os.path.exists(self.filepath)
 
     def keyset(self):
+        'return set of keys in the disk file'
         result = set()
         if self.file_exists():
             for k, v in self.items():
-                assert k not in result, 'duplicate key: %s' % str(k)
+                # assert k not in result, 'duplicate key: %s' % str(k)
                 result.add(k)
         return result
 
@@ -62,7 +63,7 @@ class Test(unittest.TestCase):
         path = '/tmp/DiskDictionary'
         os.system('rm %s' % path)
         dd = DiskDictionary('/tmp/DiskDictionary')
-        dd.append('key1', ['value1a', 27])
+        dd.append('key1', 'rec1')
         dd.close()
         dd = DiskDictionary(path)
         found = 0
@@ -72,9 +73,8 @@ class Test(unittest.TestCase):
             self.assertTrue(isinstance(k, str))
             self.assertEqual(k, 'key1')
 
-            self.assertTrue(isinstance(v, list))
-            self.assertEqual(v[0], 'value1a')
-            self.assertEqual(v[1], 27)
+            self.assertTrue(isinstance(v, str))
+            self.assertEqual(v, 'rec1')
         self.assertEqual(found, 1)
         dd.close()
 
@@ -82,10 +82,12 @@ class Test(unittest.TestCase):
         path = '/tmp/DiskDictionary'
         with DiskDictionary(path) as dd:
             dd.append('key1', ['value1', 1])
-            dd.append('key2', ['valye2', 2])
+            dd.append('key2', ['value2', 2])
         with DiskDictionary(path) as dd:
             keyset = dd.keyset()
             self.assertEqual(len(keyset), 2)
+            self.assertTrue('key1' in keyset)
+            self.assertTrue('key2' in keyset)
 
     def test_file_exists(self):
         path = '/tmp/blah blah'
