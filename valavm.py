c8c4245028d7189236e7d60bc9cff1f22e6686a0
Roy Lowrance <roy.lowrance@gmail.com>
2016-02-14 20:57:06 -0500

    make HP grid explicit in invocation

--- valavm.py
+++ valavm.py
@@ -2,14 +2,17 @@
 for AVMs based on 3 models (linear, random forests, gradient boosting regression)
 
 INVOCATION
-  python valavm.py test_month [--in PATH_IN] [--out PATH_OUT] [--test]
+  python valavm.py TEST_MONTH SWEEP [--test]
   where
-   testingMonth: yyyymm  Month of test data; training uses months just prior
-  defaults:
-      PATH_IN  ../data/working/samples-train.csv
-      PATH_OUT ../data/working/valavm/YYYYYMM.pickle
-  Note: If PATH_OUT exists, extend it (providing checkpoint-restart)
+   TEST_MONTH: yyyymm  Month of test data; training uses months just prior
+   GRID_NAME : name    Name of hyperparameter grid used; one of {"roy", "anil"}
 
+INPUTS
+ WORKING/samples-train.csv
+
+OUTPUTS
+ WORKING/valavm-GRID_NAME/TEST_MONTH.pickle
+ WORKING/valavm-GRID_NAME/log-TEST_MONTH.txt
 '''
 
 from __future__ import division
@@ -45,7 +48,7 @@ def usage(msg=None):
     sys.exit(1)
 
 
-def make_grid():
+def make_grid_anil():
     # return Bunch of hyperparameter settings
     return Bunch(
         # HP settings to test across all models
@@ -59,12 +62,35 @@ def make_grid():
 
         # HP settings to test for tree-based models
         # settings based on Anil Kocak's recommendations
-        # n_estimators_seq=(10, 30, 100, 300),
         n_estimators_seq=(300,),  # largest should be best, except for noise in the signal
-        # max_features_seq=(1, 'log2', 'sqrt', 'auto'),
         max_features_seq=('log2', 'sqrt', 'auto'),  # auto --> max features
-        # max_depth_seq=(1, 3, 10, 30, 100, 300),
         max_depth_seq=(None,),  # None --> leaves are split until pure
+
+        # HP setting to test for GradientBoostingRegression models
+        learning_rate_seq=(.10, .25, .50, .75, .99),
+        # experiments demonstrated that the best loss is seldom quantile
+        # loss_seq=('ls', 'quantile'),
+        loss_seq=('ls',),
+    )
+
+
+def make_grid_roy():
+    # return Bunch of hyperparameter settings
+    return Bunch(
+        # HP settings to test across all models
+        n_months_back_seq=(1, 2, 3, 6, 12),
+
+        # HP settings to test for ElasticNet models
+        alpha_seq=(0.01, 0.03, 0.1, 0.3, 1.0),  # multiplies the penalty term
+        l1_ratio_seq=(0.0, 0.25, 0.50, 0.75, 1.0),  # 0 ==> L2 penalty, 1 ==> L1 penalty
+        units_X_seq=('natural', 'log'),
+        units_y_seq=('natural', 'log'),
+
+        # HP settings to test for tree-based models
+        # settings based on Anil Kocak's recommendations
+        n_estimators_seq=(10, 30, 100, 300),
+        max_features_seq=(1, 'log2', 'sqrt', 'auto'),
+        max_depth_seq=(1, 3, 10, 30, 100, 300),
 
         # HP setting to test for GradientBoostingRegression models
         learning_rate_seq=(.10, .25, .50, .75, .99),
@@ -89,24 +115,25 @@ def make_control(argv):
     arg = Bunch(
         base_name='valavm',
         test_month=argv[1],
-        path_in=pcl.get_arg('--in'),
-        path_out=pcl.get_arg('--out'),
+        grid_name=argv[2],
         test=pcl.has_arg('--test'),
     )
 
-    # set defaults
-    if arg.path_in is None:
-        arg.path_in = '../data/working/samples-train.csv'
-    if arg.path_out is None:
-        arg.path_out = '../data/working/valavm/' + arg.test_month + '.pickle'
-
-    check_is_string(arg.path_in, 'PATH_IN')
-    check_is_string(arg.path_out, 'PATH_OUT')
+    assert len(arg.test_month) > 0, argv
+    assert len(arg.grid_name) > 0, argv
 
     try:
         arg.test_month = int(arg.test_month)
     except:
         usage('test_month not an integer like YYYYMM')
+
+    the_grid = None
+    if arg.grid_name == 'anil':
+        the_grid = make_grid_anil()
+    elif arg.grid_name == 'roy':
+        the_grid = make_grid_roy()
+    else:
+        usage('GRID_NAME must be one of {"anil", "roy"}')
 
     random_seed = 123
     random.seed(random_seed)
@@ -116,22 +143,17 @@ def make_control(argv):
     debug = False
 
     # assure output directory exists
-    dir_path = dir_working + arg.base_name + '/'
+    dir_path = dir_working + arg.base_name + arg.grid_name + '/'
     if not os.path.exists(dir_path):
         os.makedirs(dir_path)
-
-    fixed_hps = Bunch(
-        loss='quantile',
-        alpha=0.5,
-        n_estimators=1000,
-        max_depth=3,
-        max_features=None)
 
     return Bunch(
         arg=arg,
         debug=debug,
-        fixed_hps=fixed_hps,
-        grid=make_grid(),
+        path_in=dir_working + 'samples-train.csv',
+        path_out_file=dir_path + str(arg.test_month) + '.pickle',
+        path_out_log=dir_path + 'log-' + str(arg.test_month) + '.txt',
+        grid=the_grid,
         random_seed=random_seed,
         test=arg.test,
     )
@@ -309,21 +331,21 @@ def main(argv):
     control = make_control(argv)
     if False:
         # avoid error in sklearn that requires flush to have no arguments
-        sys.stdout = Logger(base_name=control.arg.base_name)
+        sys.stdout = Logger(log_file_path=control.path_out_log)
     print control
 
     samples = pd.read_csv(
-        control.arg.path_in,
+        control.path_in,
         nrows=None if control.test else None,
     )
     print 'samples.shape', samples.shape
 
     # assure output file exists
-    if not os.path.exists(control.arg.path_out):
-        os.system('touch %s' % control.arg.path_out)
+    if not os.path.exists(control.path_out_file):
+        os.system('touch %s' % control.path_out_file)
 
     existing_keys = set()
-    with open(control.arg.path_out, 'rb') as prior:
+    with open(control.path_out_file, 'rb') as prior:
         while True:
             try:
                 record = pickle.load(prior)
@@ -337,7 +359,7 @@ def main(argv):
                 break
     print 'number of existing keys in output file:', len(existing_keys)
 
-    with open(control.arg.path_out, 'ab') as output:
+    with open(control.path_out_file, 'ab') as output:
         def already_exists(key):
             return key in existing_keys
 
