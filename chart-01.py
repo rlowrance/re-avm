655726d145136beacfe80a6c18c41fad78939e36
Roy Lowrance <roy.lowrance@gmail.com>
2016-04-12 11:05:55 -0400

    also print 2008 stats in selective table
    Refactor printing of stats

--- chart-01.py
+++ chart-01.py
@@ -7,7 +7,7 @@ INPUT FILES
  INPUT/samples-train-validate.csv
 
 OUTPUT FILES
- WORKING/chart-01/data.pickle
+ WORKING/chart-01/data.pickle   # dict: keys=ReductionKey values=ReductionValue
  WORKING/chart-01/median-price.pdf
  WORKING/chart-01/median-price.txt
  WORKING/chart-01/median-price_2006_2007.txt
@@ -34,6 +34,9 @@ from Path import Path
 from Report import Report
 import layout_transactions as t
 cc = columns_contain
+
+ReductionKey = collections.namedtuple('ReductionKey', 'year month')
+ReductionValue = collections.namedtuple('ReductionValue', 'count mean median standarddeviation')
 
 
 def make_control(argv):
@@ -66,21 +69,16 @@ def make_control(argv):
         base_name=base_name,
         debug=debug,
         path_in_samples=dir_working + 'samples-train-validate.csv',
-        path_out_pdf=dir_path + 'median-price.pdf',
-        path_out_txt=dir_path + 'median-price.txt',
-        path_out_txt_2006_2007=dir_path + 'median-price_2006_2007.txt',
+        path_out_graph=dir_path + 'median-price.pdf',
+        path_out_stats_all=dir_path + 'price-stats-all.txt',
+        path_out_stats_2006_2008=dir_path + 'price-stats-2006-2008.txt',
         path_reduction=dir_path + reduced_file_name,
         random_seed=random_seed,
         test=arg.test,
     )
 
 
-def key(year, month):
-    return year * 100 + month
-
-
-def make_chart_pdf(data, control):
-    counts, means, medians, prices = data
+def make_chart_graph(data, control):
     years = (2003, 2004, 2005, 2006, 2007, 2008, 2009)
     months = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
     months_2009 = (1, 2, 3)
@@ -92,7 +90,7 @@ def make_chart_pdf(data, control):
     y = []
     for year in years:
         for month in (months_2009 if year == 2009 else months):
-            y.append(medians[key(year, month)])
+            y.append(data[ReductionKey(year, month)].median)
     plt.plot(x, y)
     x_ticks = [year_month[i] if i % 12 == 0 else ' '
                for i in xrange(len(year_month))
@@ -108,55 +106,52 @@ def make_chart_pdf(data, control):
     plt.xlabel('year-month')
     plt.ylabel('median price ($)')
     plt.ylim([0, 700000])
-    plt.savefig(control.path_out_pdf)
+    plt.savefig(control.path_out_graph)
 
     plt.close()
 
 
-def make_chart_txt(data, control):
-    counts, means, medians, prices = data
+def make_chart_stats(data, control, filter):
+    'return Report with statistics for years and months that obey the filter'
     r = Report()
-    format_header = '%7s %7s %7s %7s %7s'
-    format_detail = '%7d %7d %7.0f %7.0f %7d'
+    format_header = '%9s %9s %9s %9s %9s %9s'
+    format_detail = '%9d %9d %9.0f %9.0f %9d %9.0f'
     r.append('Prices by Month')
     r.append('')
-    r.append(format_header % (' ', ' ', 'mean', 'median', 'number'))
-    r.append(format_header % ('year', 'month', 'price', 'price', 'trades'))
+    r.append(format_header % (' ', ' ', 'mean', 'median', 'number', 'standard'))
+    r.append(format_header % ('year', 'month', 'price', 'price', 'trades', 'deviation'))
     for year in xrange(2003, 2010):
-        for month in (xrange(1, 4) if year == 2009 else xrange(1, 13)):
-            k = key(year, month)
-            r.append(format_detail % (
-                year,
-                month,
-                means[k],
-                medians[k],
-                counts[k],
-            ))
-    r.write(control.path_out_txt)
-
-
-def make_chart_txt_2006_2007(data, control):
-    counts, means, medians, prices = data
-    r = Report()
-    format_header = '%7s %7s %7s %7s'
-    format_detail = '%7d %7d %7.0f %7d'
-    r.append('Median Prices and Transaction Counts by Month')
-    r.append('')
-    r.append(format_header % (' ', ' ', 'median', 'number'))
-    r.append(format_header % ('year', 'month', 'price', 'trades'))
-    for year in (2006, 2007):
         for month in xrange(1, 13):
-            k = key(year, month)
-            r.append(format_detail % (
-                year,
-                month,
-                medians[k],
-                counts[k],
-            ))
-    r.write(control.path_out_txt_2006_2007)
+            if filter(year, month):
+                value = data[ReductionKey(year, month)]
+                r.append(format_detail % (
+                    year,
+                    month,
+                    value.mean,
+                    value.median,
+                    value.count,
+                    value.standarddeviation,
+                ))
+    return r
 
 
-ReductionValue = collections.namedtuple('ReductionValue', 'count mean median standarddeviation')
+def make_chart_stats_all(data, control):
+    def filter(year, month):
+        if year == 2009:
+            return 1 <= month <= 3
+        else:
+            return True
+
+    r = make_chart_stats(data, control, filter)
+    r.write(control.path_out_stats_all)
+
+
+def make_chart_stats_2006_2008(data, control):
+    def filter(year, month):
+        return year in (2006, 2007, 2008)
+
+    r = make_chart_stats(data, control, filter)
+    r.write(control.path_out_stats_2006_2008)
 
 
 def make_data(control):
@@ -168,15 +163,20 @@ def make_data(control):
     # value = list of prices
     prices = collections.defaultdict(list)
     for index, row in transactions.iterrows():
-        yyyymm = row[t.yyyymm]
+        yyyymm = row[t.yyyymm]  # an int
         prices[yyyymm].append(row[t.price])
 
     # build dictionary of statistics of prices in each period
     # key = yyyymm (an int)
     # value = ReductionValue(...)
+    def make_reduction_key(yyyymm):
+        yyyy = yyyymm // 100
+        mm = yyyymm % 100
+        return ReductionKey(yyyy, mm)
+
     reduction = {}
     for yyyymm, prices in prices.iteritems():
-        reduction[yyyymm] = ReductionValue(
+        reduction[make_reduction_key(yyyymm)] = ReductionValue(
             count=len(prices),
             mean=np.mean(prices),
             median=np.median(prices),
@@ -197,9 +197,9 @@ def main(argv):
     else:
         with open(control.path_reduction, 'rb') as f:
             data, reduction_control = pickle.load(f)
-        make_chart_txt_2006_2008(data, control)
-        make_chart_txt(data, control)
-        make_chart_pdf(data, control)
+        make_chart_stats_2006_2008(data, control)
+        make_chart_stats_all(data, control)
+        make_chart_graph(data, control)
 
     print control
     if control.test:
