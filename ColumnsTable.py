ba4660708fb222a11644399869a75a0d795ebb2b
Roy Lowrance <roy.lowrance@gmail.com>
2016-04-26 09:50:28 -0400

    change API

--- ColumnsTable.py
+++ ColumnsTable.py
@@ -24,33 +24,25 @@ class ColumnsTable(object):
         self._number_of_header_lines = 0
         for column_def in column_defs:
             cd3 = column_def[3]
-            header = cd3 if isinstance(cd3, list) else (cd3,)
+            # convert 'abc' to ('abc',)
+            headers = cd3 if isinstance(cd3, collections.Iterable) else (cd3,)
             if self._number_of_header_lines != 0:
-                if self._number_of_header_lines != len(header):
+                if self._number_of_header_lines != len(headers):
                     print 'inconsistent number of lines in header'
-                    print 'have both %d and %d' % (self._number_of_header_lines, len(header))
-                    print 'found at header: %s' % header
+                    print 'have both %d and %d' % (self._number_of_header_lines, len(headers))
+                    print 'found at headesr: %s' % headers
                     pdb.set_trace()
-            self._number_of_header_lines = len(header)
+            self._number_of_header_lines = len(headers)
             self._column_defs.append(ColumnsTableFields(column_def[0],
                                                         column_def[1],
                                                         column_def[2],
-                                                        header,
+                                                        headers,
                                                         column_def[4]))
         self._lines = []
         self._verbose = verbose
         self._header()
 
-    def _print(self):
-        for line in self._lines:
-            print line
-
-    def append(self, line):
-        if self._verbose:
-            print line
-        self._lines.append(line)
-
-    def append_legend(self):
+    def append_legend_lines(self):
         def cat_headers(headers):
             text = ''
             for header in headers:
@@ -59,29 +51,17 @@ class ColumnsTable(object):
                 text += header.strip()
             return text
 
-        self.append(' ')
-        self.append('column legend:')
+        self._append_line(' ')
+        self._append_line('column legend:')
         for cd in self._column_defs:
             line = '%12s -> %s' % (cat_headers(cd.headers), cd.legend)
-            self.append(line)
+            self._append_line(line)
 
-    def iterate_lines(self, func, **kwds):
+    def iterlines(self):
         for line in self._lines:
-            func(line, kwds)
+            yield line
 
-    def _header(self):
-        def append_header(index):
-            line = ''
-            for cd in self._column_defs:
-                formatter = '%' + str(cd.width) + 's'
-                formatted = formatter % cd.headers[index]
-                line += (' ' if len(line) > 0 else '') + formatted
-            self.append(line)
-
-        for header_line_index in xrange(self._number_of_header_lines):
-            append_header(header_line_index)
-
-    def append_detail(self, **kwds):
+    def append_detail_line(self, **kwds):
         line = ''
         for cd in self._column_defs:
             if cd.name in kwds:
@@ -91,7 +71,28 @@ class ColumnsTable(object):
             if len(line) > 0:
                 line += ' '
             line += glyph
-        self.append(line)
+        self._append_line(line)
+
+    def _append_line(self, line):
+        if self._verbose:
+            print line
+        self._lines.append(line)
+
+    def _header(self):
+        def append_header(index):
+            line = ''
+            for cd in self._column_defs:
+                formatter = '%' + str(cd.width) + 's'
+                formatted = formatter % cd.headers[index]
+                line += (' ' if len(line) > 0 else '') + formatted
+            self._append_line(line)
+
+        for header_line_index in xrange(self._number_of_header_lines):
+            append_header(header_line_index)
+
+    def _print(self):
+        for line in self._lines:
+            print line
 
 
 class TestColumnsTable(unittest.TestCase):
@@ -108,29 +109,28 @@ class TestColumnsTable(unittest.TestCase):
         self.assertTrue(True)
         pass  # tested in setUp()
 
-    def test_append(self):
+    def test_append_detail_line(self):
         c = self.columns
-        c.append_detail(a=10, bcd=20)
-        c.append_detail(bcd=30)
-        c.append_detail(a=50)
+        c.append_detail_line(a=10, bcd=20)
+        c.append_detail_line(bcd=30)
+        c.append_detail_line(a=50)
         self.assertEqual(len(c._lines), 5)
         self.assertTrue(True)
 
-    def test_iterate_lines(self):
+    def test_iterlines(self):
         c = self.columns
         counter = collections.Counter()
 
-        def handle(line, kwds):
+        for line in c.iterlines():
+            counter['line'] += 1
             if self.verbose:
                 print line
-            kwds['counter']['line'] += 1
 
-        c.iterate_lines(handle, counter=counter)
         self.assertEqual(counter['line'], 2)  # only 2 column headers lines
 
-    def test_legend(self):
+    def test_append_legend_lines(self):
         c = self.columns
-        c.append_legend()
+        c.append_legend_lines()
         self.assertTrue(True)
 
 
