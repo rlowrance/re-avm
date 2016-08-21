f8c05634c4adbd72af1aba69dddddb391fd1da21
Roy Lowrance <roy.lowrance@gmail.com>
2016-08-21 10:19:13 -0400

    select cities of interest

---
+++ interesting_cities.py
@@ -0,0 +1,15 @@
+'main program: produced list of interesting cities'
+
+# Use results from chart01/price-statistics
+
+# cities with a largest number of transactions
+print 'LOS ANGELES'
+print 'LONG BEACH'
+
+# cities with lowest median prices and at least 2,000 transactions
+print 'LANCASTER'
+print 'PALMDALE'
+
+# cities with highest median prices and at least 200 transactions
+print 'MALIBU'
+print 'PACIFIC PALISADES'
