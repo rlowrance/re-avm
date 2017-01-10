f344cc67bba5d9203e4c3648db645d04d37eaaea
Roy Lowrance <roy.lowrance@gmail.com>
2017-01-10 17:53:23 -0500

    find duplication transactions (apn|date)

--- samples-train-analysis.py
+++ samples-train-analysis.py
@@ -13,7 +13,9 @@ OUTPUT FILES:
 import argparse
 import collections
 import math
+import multiprocessing
 import numpy as np
+import os
 import pandas as pd
 import pdb
 import random
@@ -43,7 +45,7 @@ def make_control(argv):
     random.seed(random_seed)
 
     dir_working = Path.Path().dir_working()
-    path_out_dir = dirutility.assure_exists(dir_working + arg.me + ('-test/' if arg.test else '') + '/')
+    path_out_dir = dirutility.assure_exists(dir_working + arg.me + ('-test' if arg.test else '') + '/')
     return Bunch.Bunch(
         arg=arg,
         path_in_samples=dir_working + 'samples-train.csv',
@@ -72,42 +74,165 @@ def column_names():
         )
 
 
-def make_transactions(df, test):
-    'return (df of transaction IDs and prices, set(key-date) of duplicates)'
+def create_new_row(apn, date, sequence_number, row, column):
+    'return DataFrame with one row'
+    date = int(date)
+    date_year = int(date / 10000)
+    date_month = int((date - date_year * 10000) / 100)
+    date_day = int(date - date_year * 10000 - date_month * 100)
+    assert date == date_year * 10000 + date_month * 100 + date_day, date
+    new_df = pd.DataFrame(
+        data={
+            'apn': int(apn),
+            'date': date,
+            'year': date_year,
+            'month': date_month,
+            'day': date_day,
+            'sequence_number': sequence_number,
+            'actual_price': row[column.actual_price],
+        },
+        index=[make_index(apn, date, sequence_number)],
+    )
+    return new_df
+
+
+class DuplicateFinder(object):
+    def __init__(self, df, column):
+        self.df = df
+        self.column = column
+
+    def find_duplicates_method(self, apn_date):
+        mask_apn = self.df[self.column.apn] == apn_date.apn
+        mask_date = self.df[self.column.date] == apn_date.date
+        mask = mask_apn & mask_date
+        df_apn_date = self.df[mask]
+        sequence_number = 0
+        result_duplicates = set()
+        result_df = None
+        for i, row in df_apn_date.iterrows():
+            if sequence_number > 0:
+                result_duplicates.add(apn_date)
+            new_df = create_new_row(apn_date.apn, apn_date.date, sequence_number, row, self.column)
+            result_df = new_df if result_df is None else result_df.append(new_df, verify_integrity=True)
+            sequence_number += 1
+        return result_df, result_duplicates
+
+
+Info = collections.namedtuple('Info', 'apn_date, df, column')
+MappedItem = collections.namedtuple('MappedItem', 'transactions duplicates')
+
+
+def make_transactions_mapper(info):
+    'return (df of transactions, set of duplicates)'
+    verbose = False
+    if verbose:
+        print 'make_transactions_mapper begin', info.apn_date, 'pid', os.getpid()
+    apn_date, df, column = info.apn_date, info.df, info.column
+    mask_apn = df[column.apn] == apn_date.apn
+    mask_date = df[column.date] == apn_date.date
+    mask = mask_apn & mask_date
+    df_apn_date = df[mask]
+    sequence_number = 0
+    result_duplicates = set()
+    result_df = pd.DataFrame()
+    for label, row in df_apn_date.iterrows():
+        if verbose:
+            print 'make_transactions_mapper iter row label', info.apn_date, label
+        if sequence_number > 0:
+            result_duplicates.add(apn_date)
+        new_df = create_new_row(apn_date.apn, apn_date.date, sequence_number, row, column)
+        result_df = result_df.append(new_df, ignore_index=True, verify_integrity=True)
+        # result_df = new_df if result_df is None else result_df.append(new_df, verify_integrity=True)
+        sequence_number += 1
+    if verbose:
+        print 'make_transactions_mapper end', len(result_df), len(result_duplicates)
+    return MappedItem(
+        transactions=result_df,
+        duplicates=result_duplicates,
+        )
+
+
+def make_transactions_reducer(mapped_items, sofar_transactions=pd.DataFrame(), sofar_duplicates=set()):
+    'reduce list of results from mapper to final result'
+    for mapped_item in mapped_items:
+        transactions, duplicates = mapped_item.transactions, mapped_item.duplicates
+        sofar_transactions = sofar_transactions.append(transactions, verify_integrity=True, ignore_index=True)
+        if len(duplicates) > 0:
+            sofar_duplicates = sofar_duplicates.update(duplicates)
+            # otherwise set().update(set()) --> None (not set(), which would be reasonable)
+    return sofar_transactions, sofar_duplicates
+
+
+def make_transactions_parallel(df, test, in_parallel):
+    'return (df of transaction with unique APNs and IDs, set(APN_Date) of duplicates)'
+    # use 4 cores
+    def make_infos():
+        'return list of Info'
+        column = column_names()
+        result = []
+        for apn in set(df[column.apn]):
+            df_apn = df[df[column.apn] == apn]
+            for date in set(df_apn[column.date]):
+                result.append(Info(
+                    apn_date=(APN_Date(apn, date)),
+                    df=df,
+                    column=column,
+                ))
+        return result
+
+    all_infos = make_infos()
+    print 'len(all_infos)', len(all_infos)
+    infos = all_infos[:100] if test else all_infos
+    print 'len(infos)', len(infos)
+
+    if in_parallel:
+        n_processes = 1
+        print 'n_processes', n_processes
+        p = multiprocessing.Pool(n_processes)
+        mapped = p.map(make_transactions_mapper, infos)
+        all_transactions, all_duplicates = make_transactions_reducer(
+            mapped,
+            sofar_transactions=pd.DataFrame(),
+            sofar_duplicates=set(),
+            )
+    else:
+        # conserve memory by processing each info one by one
+        verbose = False
+        all_transactions = pd.DataFrame()
+        all_duplicates = set()
+        for info in infos:
+            if verbose:
+                print all_transactions
+                print all_duplicates
+            mapped_item = make_transactions_mapper(info)
+            if verbose:
+                print mapped_item.transactions
+                print mapped_item.duplicates
+                print len(mapped_item)
+            all_transactions, all_duplicates = make_transactions_reducer(
+                [mapped_item],
+                sofar_transactions=all_transactions,
+                sofar_duplicates=all_duplicates,
+                )
+            assert len(all_transactions) > 0
+            assert all_duplicates is not None
+    return all_transactions, all_duplicates
+
+
+def make_transactionsOLD(df, test):
+    'return (df of transaction IDs and prices, set(APN_Date) of duplicates)'
     column = column_names()
-    result = None
-    duplicates = set()
+    result_df = None
+    result_set = set()
     for apn in set(df[column.apn]):
-        if test and len(duplicates) > 10:
+        if test and len(result_set) > 10:
             break
         df_apn = df[df[column.apn] == apn]
         for date in set(df_apn[column.date]):
-            df_apn_date = df_apn[df_apn[column.date] == date]
-            sequence_number = 0
-            for i, row in df_apn_date.iterrows():
-                if sequence_number > 0:
-                    print 'duplicate apn|date', apn, date
-                    duplicates.add(APN_Date(apn, date))
-                date = int(date)
-                date_year = int(date / 10000)
-                date_month = int((date - date_year * 10000) / 100)
-                date_day = int(date - date_year * 10000 - date_month * 100)
-                assert date == date_year * 10000 + date_month * 100 + date_day, date
-                new_df = pd.DataFrame(
-                    data={
-                        'apn': int(apn),
-                        'date': date,
-                        'year': date_year,
-                        'month': date_month,
-                        'day': date_day,
-                        'sequence_number': sequence_number,
-                        'actual_price': row[column.actual_price],
-                        },
-                    index=[make_index(apn, date, sequence_number)],
-                    )
-                result = new_df if result is None else result.append(new_df, verify_integrity=True)
-                sequence_number += 1
-    return result, duplicates
+            duplicates_df, duplicates_set = make_transactions_mapper(APN_Date(apn, date), df, column)
+            result_df = duplicates_df if result_df is None else result_df.append(duplicates_df, verify_integrity=True, ignore_index=True)
+            result_set.append(duplicates_set)
+    return result_df, result_set
 
 
 def make_how_different(df, duplicates):
@@ -189,7 +314,13 @@ def do_work(control):
     for i, column_name in enumerate(df.columns):
         print i, column_name
 
-    transactions_df, duplicates = make_transactions(df, control.test)
+    control.timer.lap('printed column names')
+    transactions_df2, duplicates2 = make_transactions_parallel(df, control.test, False)
+    print 'not parallel lens', len(transactions_df2), len(duplicates2)
+    control.timer.lap('make transactions without parallel')
+    transactions_df, duplicates = make_transactions_parallel(df, control.test, True)
+    print 'parallel lens', len(transactions_df), len(duplicates)
+    control.timer.lap('make  transactions in parallel')
     print 'number of duplicate apn|date values', len(duplicates)
     print 'number of training samples', len(df)
     print 'number of unique apn-date-sequence_numbers', len(transactions_df)
