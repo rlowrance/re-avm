c32467feb9bcc060ec9edbc927aa2db29d2bfe0c
Roy Lowrance <roy.lowrance@gmail.com>
2017-01-12 12:16:50 -0500

    convert to module of functions

--- HPs.py
+++ HPs.py
@@ -3,186 +3,124 @@ import pdb
 import unittest
 
 
-class HPs(object):
-    names = (
-        'alpha',
-        'l1_ratio',
-        'learning_rate',
-        'max_depth',
-        'max_features',
-        'n_estimators',
-        'n_months_back',
-        'units_X',
-        'units_y',
-    )
+values = {
+    'alpha': (0.01, 0.03, 0.1, 0.3, 1.0),
+    'l1_ratio': (0.0, 0.25, 0.50, 0.75, 1.0),
+    'learning_rate': (.10, .25, .50, .75, .99),
+    'max_depth': (1, 3, 10, 30, 100, 300),
+    'max_features': (1, 'log2', 'sqrt', 'auto'),
+    'n_estimators': (10, 30, 100, 300),
+    'n_months_back': (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 18, 24),
+    'units_X': ('natural', 'log'),
+    'units_y': ('natural', 'log'),
+}
 
-    def __init__():
-        assert False, 'do not instantiate me'
 
-    @staticmethod
-    def to_str(d):
-        'return string with hyperparameters in canonical order'
-        pdb.set_trace()
-        print d
-        result = ''
-        for name in HPs.names:
-            spacer = '-' if len(result) > 0 else ''
-            if name in d:
-                value = d[name]
-                print name, value
-                if isinstance(value, float):
-                    # assure no loss of precision
-                    printed_value = '%4.2f' % value
-                    assert float(printed_value) == value, (value, printed_value)
-                elif isinstance(value, int):
-                    printed_value = '%d' % value
-                    assert int(printed_value) == value, (value, printed_value)
-                else:
-                    printed_value = value
-                result += spacer + printed_value
+def to_str(d):
+    'return string with hyperparameters in canonical order'
+    pdb.set_trace()
+    print d
+    result = ''
+    for name in values.keys():
+        spacer = '-' if len(result) > 0 else ''
+        if name in d:
+            value = d[name]
+            print name, value
+            if isinstance(value, float):
+                # assure no loss of precision
+                printed_value = '%4.2f' % value
+                assert float(printed_value) == value, (value, printed_value)
+            elif isinstance(value, int):
+                printed_value = '%d' % value
+                assert int(printed_value) == value, (value, printed_value)
             else:
-                result += spacer
-            print result
-        pdb.set_trace()
-        return result
-
-    @staticmethod
-    def to_dict(s):
-        'return dictionary d, where s was created by to_str(d)'
-        pdb.set_trace()
-        values = s.split('-')
-        result = {}
-        for i, value in enumerate(values):
-            if value != '':
-                name = HPs.names[i]
-                stored_value = (
-                    value if name in ('units_X', 'units_y') else
-                    float(value) if name in ('alpha', 'l1_ratio') else
-                    int(value)
-                )
-                result[name] = stored_value
-        pdb.set_trace()
-        return result
-
-    @staticmethod
-    def iter_hps_model(model):
-        'main entry point: yield dict of hp_name: value'
-        assert model in ('en', 'gb', 'rf'), model
-        if model == 'en':
-            return HPs.iter_hps_en()
-        elif model == 'gb':
-            return HPs.iter_hps_gb()
+                printed_value = value
+            result += spacer + printed_value
         else:
-            return HPs.iter_hps_rf()
+            result += spacer
+        print result
+    pdb.set_trace()
+    return result
 
-    # methods for models
-    @staticmethod
-    def iter_hps_en():
-        for n_months_back in HPs.iter_n_months_back():
-            for alpha in HPs.iter_alpha():
-                for l1_ratio in HPs.iter_l1_ratio():
-                    for units_X in HPs.iter_units_X():
-                        for units_y in HPs.iter_units_y():
-                            yield {
-                                'n_months_back': n_months_back,
-                                'alpha': alpha,
-                                'l1_ratio': l1_ratio,
-                                'units_X': units_X,
-                                'units_y': units_y,
-                            }
 
-    @staticmethod
-    def iter_hps_gb():
-        for n_months_back in HPs.iter_n_months_back():
-            for tree_hps in HPs.iter_hps_tree():
-                for learning_rate in HPs.iter_learning_rate():
-                    tree_hps.update({
-                        'n_months_back': n_months_back,
-                        'learning_rate': learning_rate
-                    })
-                    yield tree_hps
+def to_dict(s):
+    'return dictionary d, where s was created by to_str(d)'
+    pdb.set_trace()
+    hp_names = values.keys()
+    values = s.split('-')
+    result = {}
+    for i, value in enumerate(values):
+        if value != '':
+            name = hp_names[i]
+            stored_value = (
+                value if name in ('units_X', 'units_y') else
+                float(value) if name in ('alpha', 'l1_ratio') else
+                int(value)
+            )
+            result[name] = stored_value
+    pdb.set_trace()
+    return result
 
-    @staticmethod
-    def iter_hps_rf():
-        for n_months_back in HPs.iter_n_months_back():
-            for tree_hps in HPs.iter_hps_tree():
-                tree_hps.update({
-                    'n_months_back': n_months_back,
-                })
-                yield tree_hps
 
-    @staticmethod
-    def iter_hps_tree():
-        for n_estimators in HPs.iter_n_estimators():
-            for max_features in HPs.iter_max_features():
-                for max_depth in HPs.iter_max_depth():
+def iter_hps_model(model):
+    'main entry point: yield dict of hp_name: value'
+    assert model in ('en', 'gb', 'rf'), model
+    if model == 'en':
+        return iter_hps_en()
+    elif model == 'gb':
+        return iter_hps_gb()
+    else:
+        return iter_hps_rf()
+
+
+def iter_hps_en():
+    for n_months_back in values['n_months_back']:
+        for alpha in values['alpha']:
+            for l1_ratio in values['l1_ratio']:
+                for units_X in values['units_X']:
+                    for units_y in values['units_y']:
+                        yield {
+                            'n_months_back': n_months_back,
+                            'alpha': alpha,
+                            'l1_ratio': l1_ratio,
+                            'units_X': units_X,
+                            'units_y': units_y,
+                        }
+
+
+def iter_hps_gb():
+    for n_months_back in values['n_months_back']:
+        for max_depth in values['max_depth']:
+            for max_features in values['max_features']:
+                for n_estimators in values['n_estimators']:
+                    for learning_rate in values['learning_rate']:
+                        yield {
+                            'n_months_back': n_months_back,
+                            'max_depth': max_depth,
+                            'max_features': max_features,
+                            'n_estimators': n_estimators,
+                            'learning_rate': learning_rate,
+                        }
+
+
+def iter_hps_rf():
+    for n_months_back in values['n_months_back']:
+        for max_depth in values['max_depth']:
+            for max_features in values['max_features']:
+                for n_estimators in values['n_estimators']:
                     yield {
-                        'n_estimators': n_estimators,
-                        'max_features': max_features,
+                        'n_months_back': n_months_back,
                         'max_depth': max_depth,
+                        'max_features': max_features,
+                        'n_estimators': n_estimators,
                     }
 
-    # methods for individual hyperparameters
-    @staticmethod
-    def iter_alpha():
-        'alpha multiplies the  penalty term in elastic net models'
-        for alpha in (0.01, 0.03, 0.1, 0.3, 1.0):
-            yield alpha
 
-    @staticmethod
-    def iter_l1_ratio():
-        'for elastic net: 0 ==> L2 penalty only, 1 ==> L1 penalty only'
-        for l1_ratio in (0.0, 0.25, 0.50, 0.75, 1.0):
-            yield l1_ratio
-
-    @staticmethod
-    def iter_learning_rate():
-        'for gradient bosting, the amount by which the contribution of the next tree is shrunk'
-        for learning_rate in (.10, .25, .50, .75, .99):
-            yield learning_rate
-
-    @staticmethod
-    def iter_max_depth():
-        'for tree-based methods, maximum depth of an individual tree'
-        for max_depth in (1, 3, 10, 30, 100, 300):
-            yield max_depth
-
-    @staticmethod
-    def iter_max_features():
-        'for tree-based methods, maximum number of features to consider when splitting a node'
-        for max_features in (1, 'log2', 'sqrt', 'auto'):
-            yield max_features
-
-    @staticmethod
-    def iter_n_estimators():
-        'for tree-based methods, number of trees in the ensemble'
-        for n_estimators in (10, 30, 100, 300):
-            yield n_estimators
-
-    @staticmethod
-    def iter_n_months_back():
-        'number of months of training data to use'
-        for n_months_back in (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 18, 24):
-            yield n_months_back
-
-    @staticmethod
-    def iter_units_X():
-        'for elastic net, units for the X values'
-        for units_X in ('natural', 'log'):
-            yield units_X
-
-    @staticmethod
-    def iter_units_y():
-        'for elastic net, units for the y values'
-        for units_y in ('natural', 'log'):
-            yield units_y
-
-
-class TestHPs(unittest.TestCase):
+class TestAll(unittest.TestCase):
     def test_iter_en(self):
         verbose = False
         count = 0
-        for hps in HPs.iter_hps_model('en'):
+        for hps in iter_hps_model('en'):
             count += 1
             if verbose:
                 print count, hps
@@ -191,7 +129,7 @@ class TestHPs(unittest.TestCase):
     def test_iter_gb(self):
         verbose = False
         count = 0
-        for hps in HPs.iter_hps_model('gb'):
+        for hps in iter_hps_model('gb'):
             count += 1
             if verbose:
                 print count, hps
@@ -200,7 +138,7 @@ class TestHPs(unittest.TestCase):
     def test_iter_rf(self):
         verbose = False
         count = 0
-        for hps in HPs.iter_hps_model('rf'):
+        for hps in iter_hps_model('rf'):
             count += 1
             if verbose:
                 print count, hps
