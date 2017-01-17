37acccc3733bc52a593d2792feb20025df35fb91
Roy Lowrance <roy.lowrance@gmail.com>
2017-01-17 08:20:02 -0500

    run on unix as well as windows

--- fit-train-all.py
+++ fit-train-all.py
@@ -72,21 +72,30 @@ MapResult = collections.namedtuple('MapResult', 'last_month error_level')
 def mapper(mapper_arg):
     print 'mapper', mapper_arg
     invocation = 'python fit.py train %s %s all' % (mapper_arg.model, mapper_arg.last_month)
-    # options:
-    #  /BELOWNORMAL  use BELOWNORMAL priority class
-    #  /LOW          use IDLE priority class
-    #  /WAIT         wait for app to terminae
-    window_title = str(mapper_arg.model) + str(mapper_arg.last_month)
-    command = 'START "%s" /LOW /WAIT %s' % (window_title, invocation)
+    if os.name == 'nt':
+        # options:
+        #  /BELOWNORMAL  use BELOWNORMAL priority class
+        #  /LOW          use IDLE priority class
+        #  /WAIT         wait for app to terminate
+        #  /B            start app without opening a new command window
+        # NOTE: this approach seems to start 2 processes, which is 1 too many
+        window_title = str(mapper_arg.model) + str(mapper_arg.last_month)
+        command = 'START "%s" /LOW /WAIT /B %s' % (window_title, invocation)
+    elif os.name == 'posix':
+        command = "nice 18 " + invocation  # set very low priority (19 is lowest)
+    else:
+        msg = 'unexpected os.name: ', os.name
+        print msg
+        raise RuntimeError(msg)
     print 'mapper', command
     # pdb.set_trace()
-    error_level = (
+    return_code = (
         0 if mapper_arg.test else
         subprocess.call(invocation)  # fit many models
     )
     return MapResult(
         last_month=mapper_arg.last_month,
-        error_level=error_level,
+        error_level=return_code,
     )
 
 
