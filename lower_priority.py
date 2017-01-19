0d58757d47b24a740d1c421c9c50cae68bcf0681
Roy Lowrance <roy.lowrance@gmail.com>
2017-01-19 09:53:23 -0500

    imports work for nt and posix systems

--- lower_priority.py
+++ lower_priority.py
@@ -1,14 +1,15 @@
 'lower priority of current process'
 import os
-import win32api
-import win32process
-import win32con
+import pdb
 
 
 def lower_priority():
     # ref: http://stackoverflow.com/questions/1023038/change-process-priority-in-python-cross-platform
     assert os.name in ('nt', 'posix'), os.name
     if os.name == 'nt':
+        import win32api
+        import win32process
+        import win32con
         pid = win32api.GetCurrentProcessId()
         handle = win32api.OpenProcess(win32con.PROCESS_ALL_ACCESS, True, pid)
         win32process.SetPriorityClass(handle, win32process.BELOW_NORMAL_PRIORITY_CLASS)
